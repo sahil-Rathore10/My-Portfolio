351eb26e045f3a87adb36e3acbfa20ba2fcb9c77
Sahil Rathore <rathors378@gmail.com>
2025-07-10 22:02:41 +0530

    next day

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from "react";
+import bgImage from "../assets/bg.png";
 
 const Hero = () => {
   return (
-    <div className='p-4 px-6 w-full mx-auto  flex flex-col justify-center items-center'>
-  <h2 className='text-xl mt-20 mb-0 leading-none'>👋, my name is Sahil and I am a freelancer</h2>
+    <div className="p-4 px-6 w-full relative mx-auto  flex flex-col justify-center items-center">
+      <div className="absolute inset-0 z-10 h-[350px] w-[250px] top-115 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <img
+          src={bgImage}
+          alt="bg-image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <h2 className="text-xl mt-25 mb-0 leading-none">
+        👋, my name is Sahil and I am a freelancer
+      </h2>
 
-  <h1 className="mb-0 leading-[.8] text-center px-2 text-[10vw] font-bold tracking-tighter font-['Placard next cond bold']"> Web Developer <br />  <span
-    className="text-9xl tracking-tighter text-transparent font-['Placard next cond bold'] font-bold leading-none"
-    style={{ WebkitTextStroke: '2px black' }}
-  >
-    & Building Modern Web Experiences
-  </span></h1>
+      <h1 className="mb-0 leading-[.8] text-center px-2 text-[10vw] font-bold tracking-tighter font-['Placard next cond bold']">
+        {" "}
+        Web Developer <br />{" "}
+        <span
+          className="text-9xl tracking-tighter text-transparent font-['Placard next cond bold'] font-bold leading-none"
+          style={{ WebkitTextStroke: "2px black" }}
+        >
+          & Building Modern Web Experiences
+        </span>
+      </h1>
 
-  
-  {/* <h1 className="mb-1 px-2 text-[10vw] font-bold tracking-tighter font-['Placard next cond bold'] leading-tight">
+      {/* <h1 className="mb-1 px-2 text-[10vw] font-bold tracking-tighter font-['Placard next cond bold'] leading-tight">
     Web Developer
   </h1>
   
@@ -23,9 +36,8 @@ const Hero = () => {
   >
     & Building Modern Web Experiences
   </h1> */}
-</div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
